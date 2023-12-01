ee6c9c366f6f22cd91ce2397a8c58df216ad99d6
lingenj <je.lingen@portofrotterdam.com>
2023-12-01 21:25:16 +0100

    Fix first day (second star)

--- src/main/kotlin/DayOne.kt
+++ src/main/kotlin/DayOne.kt
@@ -1,18 +1,14 @@
-tailrec fun translateToDigits(input: String, pointer: Int = 0): String =
-    if (pointer == input.length) input
-    else translateToDigits(replaceFirstDigit(input.drop(pointer), input), pointer + 1)
-
-private fun replaceFirstDigit(chunkedPart: String, input: String) =
-    if (chunkedPart.startsWith("one")) input.replaceFirst("one", "1")
-    else if (chunkedPart.startsWith("two")) input.replaceFirst("two", "2")
-    else if (chunkedPart.startsWith("three")) input.replaceFirst("three", "3")
-    else if (chunkedPart.startsWith("four")) input.replaceFirst("four", "4")
-    else if (chunkedPart.startsWith("five")) input.replaceFirst("five", "5")
-    else if (chunkedPart.startsWith("six")) input.replaceFirst("six", "6")
-    else if (chunkedPart.startsWith("seven")) input.replaceFirst("seven", "7")
-    else if (chunkedPart.startsWith("eight")) input.replaceFirst("eight", "8")
-    else if (chunkedPart.startsWith("nine")) input.replaceFirst("nine", "9")
-    else input
+fun translateToDigits(input: String) =
+    input
+        .replace("one", "one1one")
+        .replace("two", "two2two")
+        .replace("three", "three3three")
+        .replace("four", "four4four")
+        .replace("five", "five5five")
+        .replace("six", "six6six")
+        .replace("seven", "seven7seven")
+        .replace("eight", "eight8eight")
+        .replace("nine", "nine9nine")
 
 fun getFirstDecimal(input: String) =
     input.toCharArray().first { it.isDigit() }
@@ -21,24 +17,18 @@ fun getLastDecimal(input: String) =
     getFirstDecimal(input.reversed())
 
 fun main() {
-     // First star
-     val output = getInput(1)
-         .map { getFirstDecimal(it) + getLastDecimal(it) }
-         .sumOf { it.toInt() }
+    // First star
+    val output = getInput(1)
+        .map { getFirstDecimal(it) + getLastDecimal(it) }
+        .sumOf { it.toInt() }
 
-     println(output)
+    println(output)
 
-     // Second start
-     val output2 = getInput(1)
-         .map {
-             // debug logging
-             val that = translateToDigits(it)
-             println("$it -> $that -> ${getFirstDecimal(that)} & ${getLastDecimal(that)} => ${getFirstDecimal(that) + getLastDecimal(that)}")
+    // Second start
+    val output2 = getInput(1)
+        .map { translateToDigits(it) }
+        .map { getFirstDecimal(it) + getLastDecimal(it) }
+        .sumOf { it.toInt() }
 
-             translateToDigits(it)
-         }
-         .map { getFirstDecimal(it) + getLastDecimal(it) }
-         .sumOf { it.toInt() }
-
-     println(output2)
+    println(output2)
 }
